013617a54183c3ee59aa09bf7ecaaa35fc9a1e1a
Alexey Ulashchick <a.ulashchick@gmail.com>
2022-02-11 20:41:39 -0800

    Switching to NodeJS protobufs instead of browser version.

--- generateProtos.sh
+++ generateProtos.sh
@@ -10,7 +10,17 @@ cd ../env-nodejs || exit
 rm -rf protos
 npm install
 cd .. || exit
-protoc \
-  ./protos/**  \
-  --js_out="import_style=commonjs:env-nodejs" \
-  --grpc-web_out="import_style=typescript,mode=grpcweb:env-nodejs"
+# protoc \
+#   ./protos/**  \
+#   --js_out="import_style=commonjs:env-nodejs" \
+#   --grpc-web_out="import_style=typescript,mode=grpcweb:env-nodejs"
+PROTOC="./env-nodejs/node_modules/.bin/grpc_tools_node_protoc"
+PROTOC_GEN_TS_PATH="./env-nodejs/node_modules/.bin/protoc-gen-ts"
+PROTOC_GEN_GRPC_PATH="./env-nodejs/node_modules/.bin/grpc_tools_node_protoc_plugin"
+$PROTOC \
+    ./protos/** \
+    --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH \
+    --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
+    --js_out=import_style=commonjs:env-nodejs \
+    --grpc_out=grpc_js:env-nodejs \
+    --ts_out=grpc_js:env-nodejs
